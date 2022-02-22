86dadc1ed919489d2b47d1c058f561565cb935ab
dmcgithub315 <cuongdmhe150229@fpt.edu.vn>
2022-02-22 17:47:07 +0700

    update buoi 8

--- Buoi8/Bai8/index.js
+++ Buoi8/Bai8/index.js
@@ -1,17 +1,16 @@
-function soLe(n) {
-    let check = 1;
-    for (i = 1; i <= n.length; i++) {
-        n = (n % (10 * i));
-        if (n % 2 == 0) {
+function ngoc(n) {
+    var arr = n.toString(10).replace(/\D/g, '0').split('').map(Number);
+    var check = 1;
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] % 2 == 0) {
             check = 0;
             break;
         }
     }
-    if (check == false) {
-        console.log("Yes");
-    }
-    else {
-        console.log("No");
+    if (check == 1) {
+        console.log('Yes');
+    } else {
+        console.log('No');
     }
 }
-soLe(135);
+ngoc(1345);
